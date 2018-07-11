8d943770e4283caa735fc60f911d1d6c01d547eb
bengeos <george.beng@gmail.com>
2018-07-11 14:21:57 +0300

    updates

---
+++ app/Http/Controllers/NegaritSMSController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NegaritSMSController extends Controller
+{
+    protected $negarit_api_url = "http://api.negarit.net/api/";
+
+    /**
+     * NegaritSMSController constructor.
+     */
+    public function __construct()
+    {
+        $this->negarit_api_url = "http://api.negarit.net/api/";
+    }
+
+    public function sendPostRequest($request_route, $send_post_data){
+        $ch = curl_init($this->negarit_api_url.$request_route);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $send_post_data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+}
--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -7,10 +7,28 @@ use Monolog\Logger;
 
 class RegistrationController extends Controller
 {
+    protected $negaritManager;
+    /**
+     * RegistrationController constructor.
+     */
+    public function __construct()
+    {
+        $this->negaritManager = new NegaritSMSController();
+    }
+
     public function register() {
         $newDataLogger = new Logger('data_logger');
         $post_data = request()->all();
-        print_r($post_data);
         $newDataLogger->log(Logger::INFO, 'register', ['post_data'=>$post_data]);
+        if(isset($post_data['phone']) && isset($post_data['full_name'])) {
+            $send_data = array();
+            $send_data['campaign_id'] = 1;
+            $send_data['message'] = "Dear ".$post_data['full_name']."\n Welcome To Tesfa Conference!\n You are Registered For The Event";
+            $send_data['sent_to'] = $post_data['phone'];
+            $response = $this->negaritManager->sendPostRequest("api_request/sent_message?API_KEY=ZbaFOaXNUocbBpFBzJTqVg5g1gLGZRd7", $send_data);
+            return response()->json(['status'=>true, 'message'=> "Your Are Successfully Registered", 'error'=>"We have sent you confirmation SMS", "post_data"=>$post_data],200);
+        } else {
+
+        }
     }
 }
